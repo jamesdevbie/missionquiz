f564635dbf15fa3fdd4407236f3b128f3d560c3e
jamesdevbie <jamesdevbie@gmail.com>
2025-08-22 12:09:43 +0530

    Updated CountDown Tpes

--- missionquiz/src/CountDown/CountDown.tsx
+++ missionquiz/src/CountDown/CountDown.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import './CountDown.css'
 interface CountDownProps {
-  submitHandler: () => void
-  nextHandler?: () => void
+  submitHandler: (e: React.MouseEvent<HTMLButtonElement>) => void
+  nextHandler?: (e: React.MouseEvent<HTMLButtonElement>) => void
   startCounter: boolean
   displayTimer: string
 }
